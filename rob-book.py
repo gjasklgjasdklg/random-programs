91a6ff7043412731c6999c058e0b03db783ad8a6
gjasklgjasdklg <18colemanto@olympustrust.co.uk>
2022-01-28 12:39:57 +0000

    edit robbook

--- rob-book.py
+++ rob-book.py
@@ -16,4 +16,4 @@ chosen_guru = input("Enter a username: ")
 if chosen_guru in coding_gurus:
     print("The guru " + chosen_guru + " will be in contact shortly.")
 else:
-    print("That username does not belong to a coding guru.")
+    print("The username "+ chosen_guru + " does not belong to a coding guru.")
