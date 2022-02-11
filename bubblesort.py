7af941559d2eeb195d5e01e7d5048a6401e74329
gjasklgjasdklg <18colemanto@olympustrust.co.uk>
2022-02-11 12:26:28 +0000

    make bubble sort more efficient

--- bubblesort.py
+++ bubblesort.py
@@ -1,14 +1,15 @@
 def bubbleSort(array):
     swap = True
+    end = len(array)
     while swap == True:
         swap = False
-        for i in range(1, len(array)):
+        for i in range(1, end):
             if array[i-1] > array[i]:
                 temp = array[i]
                 array[i] = array[i-1]
                 array[i-1] = temp
                 swap = True
-
+        end -= 1
 examScores = [92,55,84,48,72,90,87,54,67,75]
 print(examScores)
 bubbleSort(examScores)
